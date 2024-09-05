c510937419fcd622757fd44e7f00a6e984fe99e9
reychman <reychman@gmail.com>
2024-09-05 07:45:07 -0400

    modificamos la consulta en mapa_busquedas.php y en el mapa_busquedas_page.dart agregamos un alertDialog, y ahora se puede ver la foto, nombre y descripcion de la mascota e ir a la pagina infoMascotaPage

--- homecomingbd_v2/mapa_busquedas.php
+++ homecomingbd_v2/mapa_busquedas.php
@@ -6,7 +6,12 @@ header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorizatio
 header('Content-Type: application/json');
 include 'config.php';
 
-$sql = "SELECT id, especie, descripcion, latitud, longitud FROM mascotas WHERE estado = 'perdido' AND estado_registro = 1";
+//$sql = "SELECT id, especie, descripcion, latitud, longitud FROM mascotas WHERE estado = 'perdido' AND estado_registro = 1";
+$sql = "SELECT m.id, m.nombre, m.especie, m.raza, m.sexo, m.fecha_perdida, m.lugar_perdida, m.estado, m.descripcion, m.foto, m.latitud, m.longitud,u.nombre AS nombre_dueno,
+        u.email AS email_dueno, u.telefono AS telefono_dueno
+    FROM mascotas m
+    JOIN usuarios u ON m.usuario_id = u.id
+    WHERE m.estado = 'perdido' AND m.estado_registro = 1";
 $result = $conexion->query($sql);
 
 $mascotas = array();
--- lib/pages/menu/mapa_busquedas_page.dart
+++ lib/pages/menu/mapa_busquedas_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:homecoming/ip.dart';
 import 'package:homecoming/pages/mascota.dart';
+import 'package:homecoming/pages/menu/info_mascotas_page.dart';
 import 'package:homecoming/pages/menu/menu_widget.dart';
 import 'package:homecoming/pages/usuario.dart';
 import 'dart:convert'; // Importa para decodificar JSON
@@ -13,10 +14,9 @@ class MapaBusquedasPage extends StatefulWidget {
 }
 
 class _MapaBusquedasPageState extends State<MapaBusquedasPage> {
-  late GoogleMapController _mapController;
+  GoogleMapController? _mapController;
   BitmapDescriptor? _perroIcon;
   BitmapDescriptor? _gatoIcon;
-  List<Mascota> _mascotas = [];
 
   final CameraPosition _initialPosition = CameraPosition(
     target: LatLng(-17.3957147, -66.1581871),
@@ -27,16 +27,19 @@ class _MapaBusquedasPageState extends State<MapaBusquedasPage> {
   void initState() {
     super.initState();
     _loadCustomMarkers();
-    _fetchMascotas();
+    _fetchMascotas(); // Cargar mascotas desde la base de datos
   }
+
+  Usuario? usuario;
+  List<Mascota> _mascotas = [];
 
   Future<void> _loadCustomMarkers() async {
     _perroIcon = await BitmapDescriptor.fromAssetImage(
-      const ImageConfiguration(size: Size(30, 30)),
+      ImageConfiguration(size: Size(48, 48)),
       'assets/imagenes/perro.png',
     );
     _gatoIcon = await BitmapDescriptor.fromAssetImage(
-      const ImageConfiguration(size: Size(39, 39)),
+      ImageConfiguration(size: Size(48, 48)),
       'assets/imagenes/gato.png',
     );
     setState(() {});
@@ -57,15 +60,22 @@ Future<void> _fetchMascotas() async {
 
 
   void _onMapCreated(GoogleMapController controller) {
-    _mapController = controller;
-    _mapController.animateCamera(
-      CameraUpdate.newCameraPosition(
-        CameraPosition(
-          target: LatLng(-17.3957147, -66.1581871),
-          zoom: 14,
+    setState(() {
+      _mapController = controller;
+    });
+    _animateToInitialPosition(); // Llama a la función para animar la cámara
+  }
+  void _animateToInitialPosition() {
+    if (_mapController != null) {
+      _mapController!.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(-17.3957147, -66.1581871),
+            zoom: 14,
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 
   @override
@@ -74,12 +84,12 @@ Future<void> _fetchMascotas() async {
       appBar: AppBar(
         title: Text('Mapa de búsquedas'),
       ),
-      drawer: MenuWidget(usuario: Usuario.vacio()),
-      body: _perroIcon == null || _gatoIcon == null
+      drawer: MenuWidget(usuario: usuario ?? Usuario.vacio()), 
+      body: (_perroIcon == null || _gatoIcon == null)
           ? Center(child: CircularProgressIndicator())
           : GoogleMap(
               initialCameraPosition: _initialPosition,
-              onMapCreated: _onMapCreated,
+              onMapCreated: _onMapCreated, // Asegúrate de asignar el método aquí
               markers: _createMarkers(),
             ),
     );
@@ -87,16 +97,95 @@ Future<void> _fetchMascotas() async {
 
   Set<Marker> _createMarkers() {
     return _mascotas.map((mascota) {
-      final icon = mascota.especie.toLowerCase() == 'perro' ? _perroIcon : _gatoIcon;
+      BitmapDescriptor? icon = (mascota.especie == 'perro') ? _perroIcon : _gatoIcon;
+      
       return Marker(
         markerId: MarkerId(mascota.id.toString()),
         position: LatLng(mascota.latitud!, mascota.longitud!),
         icon: icon ?? BitmapDescriptor.defaultMarker,
-        infoWindow: InfoWindow(
-          title: 'Aquí hay una mascota desaparecida',
-          snippet: mascota.descripcion,
-        ),
+        onTap: () => _showMascotaInfoDialog(mascota), // Mostrar el diálogo al hacer clic
       );
     }).toSet();
+  }
+
+  void _showMascotaInfoDialog(Mascota mascota) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'No me dejes solo,',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Ayúdame a encontrar el camino.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          contentPadding: EdgeInsets.all(16), // Padding alrededor del contenido
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Mostrar la foto de la mascota
+              Center(
+                child: mascota.foto.isNotEmpty
+                    ? Image.asset(
+                        'assets/imagenes/fotos_mascotas/${mascota.foto}',
+                        width: 150,
+                        height: 150,
+                        fit: BoxFit.cover,
+                      )
+                    : Icon(Icons.pets, size: 100, color: Colors.grey),
+              ),
+              SizedBox(height: 16),
+              // Mostrar el nombre de la mascota
+              Text(
+                'Nombre: ${mascota.nombre.isNotEmpty ? mascota.nombre.toUpperCase() : 'Desconocido'}',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              SizedBox(height: 8),
+              // Mostrar la descripción de la mascota
+              Text(mascota.descripcion),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  // Botón para ver más detalles
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context); // Cerrar el diálogo
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => InfoMascotasPage(mascota: mascota), // Navegar a la página de información
+                        ),
+                      );
+                    },
+                    child: Text('Ver más detalles'),
+                  ),
+                  SizedBox(width: 8), // Espacio entre los botones
+                  // Botón para cerrar el diálogo
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Cerrar'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
